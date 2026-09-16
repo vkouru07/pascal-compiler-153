7ca36354c6aa18e4cfed221e6559e50b5fb0b02a
Edmond Pan <edmond13pan@gmail.com>
2026-09-15 17:36:50 -0700

    FIxed While?

--- src/backend/interpreter/Executor.java
+++ src/backend/interpreter/Executor.java
@@ -9,6 +9,17 @@ import intermediate.symtab.*;
 
 public class Executor extends SimpleA3BaseVisitor<Object>
 {
+
+    @Override 
+    public Object visitWhileStatement(WhileStatementContext ctx)
+    {
+        while ((Boolean) visit(ctx.expression())) {
+            visit(ctx.statement());
+        }
+
+        return null;    
+    }   
+
     @Override
     public Object visitForStatement (ForStatementContext ctx) {
         visit (ctx.assignmentStatement());
@@ -188,6 +199,10 @@ public class Executor extends SimpleA3BaseVisitor<Object>
             
         if      (op.equals("=" )) return value1 == value2;
         else if (op.equals("<" )) return value1 <  value2;
+        else if (op.equals("<=")) return value1 <= value2;
+        else if (op.equals("<>")) return value1 != value2;
+        else if (op.equals(">" )) return value1 >  value2;
+        else if (op.equals(">=")) return value1 >= value2;
         
         return null;
     }
